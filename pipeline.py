b21073406b73bfa42a9039fc6d25ac872bc3aca6
techproedu <88002070+techproedu@users.noreply.github.com>
2021-07-28 13:48:36 +0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,2 +1,3 @@
 print('My first python job which is run within Jenkinsfile.')
-print('My first python job which is run within Jenkinsfile.')
+for count in 5:
+  print('My first python job which is run within Jenkinsfile.')
